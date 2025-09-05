f75ebf0a77fe49adc2a3dbca6e0f609ed962454a
Tobias Klug <tbs.klg@gmail.com>
2025-09-05 08:33:58 +0200

    Solve day 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -2,6 +2,7 @@ fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
 
     println!("part 1: {}", part_1(&input));
+    println!("part 2: {}", part_2(&input));
 }
 
 fn part_1(input: &str) -> usize {
@@ -18,6 +19,21 @@ fn part_1(input: &str) -> usize {
         .sum()
 }
 
+fn part_2(input: &str) -> usize {
+    input
+        .lines()
+        .map(map_to_dimensions)
+        .map(|(l, w, h)| {
+            let x = 2 * l + 2 * w;
+            let y = 2 * l + 2 * h;
+            let z = 2 * w + 2 * h;
+            let r = l * w * h;
+
+            x.min(y).min(z) + r
+        })
+        .sum()
+}
+
 fn map_to_dimensions(line: &str) -> (usize, usize, usize) {
     let mut elements = line.split('x').map(|x| x.parse().unwrap());
 
@@ -30,11 +46,17 @@ fn map_to_dimensions(line: &str) -> (usize, usize, usize) {
 
 #[cfg(test)]
 mod tests {
-    use crate::part_1;
+    use crate::{part_1, part_2};
 
     #[test]
     fn should_return_square_feet_of_paper() {
         assert_eq!(part_1("1x1x10"), 43);
         assert_eq!(part_1("2x3x4"), 58);
+    }
+
+    #[test]
+    fn should_return_feet_of_ribbon() {
+        assert_eq!(part_2("1x1x10"), 14);
+        assert_eq!(part_2("2x3x4"), 34);
     }
 }
