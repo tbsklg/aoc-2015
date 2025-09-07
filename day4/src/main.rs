3a3830896a7c1c925cca265e570cc4298609c624
Tobias Klug <tbs.klg@gmail.com>
2025-09-07 14:16:04 +0200

    Update day 4

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,25 +1,39 @@
+#![warn(clippy::pedantic)]
+
 use md5::{Digest, Md5};
+use rayon::iter::{IntoParallelIterator as _, ParallelIterator};
 use std::env::args;
 
 fn main() {
     let input = args().nth(1).unwrap();
 
-    println!("part 1: {}", solve(&input, 5).unwrap());
-    println!("part 1: {}", solve(&input, 6).unwrap());
+    let now = std::time::Instant::now();
+    println!(
+        "part 1: {} ({:?})",
+        solve(&input, 5).unwrap(),
+        now.elapsed()
+    );
+    println!(
+        "part 1: {} ({:?})",
+        solve(&input, 6).unwrap(),
+        now.elapsed()
+    );
 }
 
 fn solve(input: &str, leading_zeros: usize) -> Option<usize> {
-    for i in 1.. {
-        let mut hasher = Md5::new();
-        hasher.reset();
-        hasher.update(input.as_bytes());
-        hasher.update(i.to_string().as_bytes());
-        let result = hasher.finalize();
+    let target = "0".repeat(leading_zeros);
 
-        if format!("{:x}", result).starts_with(&"0".repeat(leading_zeros)) {
-            return Some(i);
-        }
-    }
+    let chunk_size = 100_000;
 
-    None
+    (1..).step_by(chunk_size).find_map(|start| {
+        (start..start + chunk_size)
+            .into_par_iter()
+            .find_first(|&i| {
+                let mut hasher = Md5::new();
+                hasher.update(input.as_bytes());
+                hasher.update(i.to_string().as_bytes());
+                let result = hasher.finalize();
+                format!("{result:x}").starts_with(&target)
+            })
+    })
 }
