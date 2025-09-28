0aab3b24b691d85ab0c9455985402f75344d0c2f
Tobias Klug <tbs.klg@gmail.com>
2025-09-28 06:30:00 +0200

    Day 9: Parse inputs

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,3 +1,5 @@
+use std::{collections::HashMap, iter::Map};
+
 fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
 
@@ -6,5 +8,27 @@ fn main() {
 }
 
 fn part_1(input: &str) -> usize {
-    todo!()
+    let distances = parse_distances(input).unwrap();
+    println!("{:?}", distances);
+    0
+}
+
+fn parse_distances(
+    input: &str,
+) -> Result<HashMap<(String, String), usize>, Box<dyn std::error::Error>> {
+    input
+        .lines()
+        .map(parse_distance)
+        .collect::<Result<HashMap<_, _>, _>>()
+}
+
+fn parse_distance(input: &str) -> Result<((String, String), usize), Box<dyn std::error::Error>> {
+    let mut parts = input.split(" to ").flat_map(|s| s.split(" = "));
+
+    let from = parts.next().ok_or("Missing first city")?;
+    let to = parts.next().ok_or("Missing second city")?;
+    let distance = parts.next().ok_or("Missing distance")?;
+    let distance = distance.parse::<usize>()?;
+
+    Ok(((from.to_string(), to.to_string()), distance))
 }
